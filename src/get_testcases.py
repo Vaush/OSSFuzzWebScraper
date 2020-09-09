3d46922b4da53b6353a53e06a4aa590db5dfd391
Dario Asprone <dario.asprone.19@ucl.ac.uk>
2020-09-09 13:01:27 +0100

    Refactoring

---
+++ src/get_testcases.py
@@ -0,0 +1,34 @@
+import wget
+import json
+import os
+failed_bugs = []
+json = json.load(open("bugs_selection"))
+for project in json:
+    for target in json[project]:
+        for version in json[project][target]:
+            name = project+"_"+target
+            dirname = "testcases/"+name
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+            dirname += "/testcases"
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+            for bug in json[project][target][version]:
+                getNext = False
+                bugfile = open("saved_bugs/"+bug["id"])
+                arr = [x for x in bugfile if x != "\n"]
+                bugfile.close()
+                address = None
+                for line in arr:
+                    if getNext:
+                        address = line
+                        break;
+                    if line == "Reproducer Testcase:\n":
+                        getNext = True
+                else:
+                    failed_bugs.append(bug["id"])
+                if address != None and not os.path.exists(dirname + "/" + bug["id"]) and not os.path.exists(dirname + "/" + bug["id"] + ".txt"):
+                    print(bug["id"] + ": " + wget.download(address, dirname +
+ "/" + bug["id"] + ".txt"))
+print("Failed bugs:\n" + str(failed_bugs))
+
